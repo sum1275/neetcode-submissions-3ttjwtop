3a526805825766965fb9d25b6c0aa9a719861a99
sum1275 <59500999+sum1275@users.noreply.github.com>
2026-04-05 16:46:26 +0530

    Add: products-of-array-discluding-self - submission-5

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-5.js
@@ -0,0 +1,22 @@
+class Solution {
+  /**
+   * @param {number[]} nums
+   * @return {number[]}
+   */
+  productExceptSelf(nums) {
+    let n = nums.length;
+    let left = new Array(n).fill(1);
+    let right = new Array(n).fill(1);
+    let ans=new Array(n);
+    for (let i = 1; i < n; i++) {
+      left[i] = left[i - 1] * nums[i - 1];
+    }
+    for (let i = n - 2; i >= 0; i--) {
+      right[i] = right[i + 1] * nums[i + 1];
+    }
+    for(let i=0;i<n;i++){
+      ans[i]=left[i]*right[i];
+    }
+    return ans;
+  }
+}
