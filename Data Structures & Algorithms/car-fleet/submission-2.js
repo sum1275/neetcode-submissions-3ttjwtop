f900cfe679f38fa4fb78da05016f34ad2ccca14b
sum1275 <59500999+sum1275@users.noreply.github.com>
2026-04-07 17:53:32 +0530

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} position
+     * @param {number[]} speed
+     * @return {number}
+     */
+    carFleet(target, position, speed) {
+        let n = position.length;
+        let cars = [];
+        for (let i = 0; i < n; i++) {
+            cars.push([position[i], speed[i]]);
+
+        }
+        cars.sort((a, b) => a[0] - b[0]);
+        let time = new Array(n);
+        for (let i = 0; i < n; i++) {
+            time[i] = (target - cars[i][0]) / cars[i][1];
+        }
+        let fleet = n;
+        for (let i = n - 1; i >= 0; i--) {
+            for (let j = i + 1; j < n; j++) {
+                if (time[i] <= time[j]) {
+                    fleet--;
+                    break;
+                }
+            }
+        }
+        return fleet;
+    }
+}
+/*We compare times instead of simulating movement. If a car behind 
+reaches earlier or same time, it merges into the fleet ahead."*/ 
