2c6a09e2cdf45f259531932687b4e691554a5723
sum1275 <59500999+sum1275@users.noreply.github.com>
2026-04-09 08:25:28 +0530

    Update: longest-substring-without-duplicates - submission-0

--- Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.js
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.js
@@ -4,15 +4,16 @@ class Solution {
      * @return {number}
      */
     lengthOfLongestSubstring(s) {
-        let left = 0, maxLen = 0;
-        let set = new Set();
-        for (let right = 0; right < s.length; right++) {
-            while(set.has(s[right])) {
-                set.delete(s[left])
-                left++;
+        let maxLen=0;
+        for(let i=0;i<s.length;i++){
+            let set=new Set();
+            for(let j=i;j<s.length;j++){
+                if(set.has(s[j])){
+                    break;
+                }
+                set.add(s[j]);
+                maxLen=Math.max(maxLen,j-i+1);
             }
-            set.add(s[right]);
-            maxLen = Math.max(maxLen, right - left + 1);
         }
         return maxLen;
     }
