b2a6a4fa95a8ef3a622442e762cb7c9457a00ae0
sum1275 <59500999+sum1275@users.noreply.github.com>
2026-04-09 11:59:52 +0530

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        let maxLen = 0;
+        for (let i = 0; i < s.length; i++) {
+            let count = {};
+            let maxFreq = 0;
+            for (let j = i; j < s.length; j++) {
+                count[s[j]] = (count[s[j]] || 0) + 1;
+                maxFreq = Math.max(maxFreq, count[s[j]]);
+                let windowSize = j - i + 1;
+                let changes = windowSize - maxFreq;
+                if (changes <= k) {
+                    maxLen = Math.max(maxLen, windowSize);
+                }
+
+
+            }
+
+
+        }
+        return maxLen;
+    }
+}
