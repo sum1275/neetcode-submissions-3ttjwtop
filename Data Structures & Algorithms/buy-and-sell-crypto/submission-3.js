ee94fbf3534630e96234d9959f80fed7317acc23
sum1275 <59500999+sum1275@users.noreply.github.com>
2026-04-09 07:53:39 +0530

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let left = 0;
+        let right = 1;
+        let maxProfit = 0;
+        while (right < prices.length) {
+            if (prices[right] > prices[left]) {
+                let profit = prices[right] - prices[left];
+                maxProfit=Math.max(maxProfit,profit);
+            }else{
+                left=right;
+            }
+            right++;
+        }
+        return maxProfit;
+    }
+}
